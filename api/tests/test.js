5fadac38357e9b6d14eb424e8eea0d48a8426352
ohanhimaki <ohanhimaki@gmail.com>
2019-11-25 19:39:34 +0200

    Tehty mochalla muutama testi apille

--- api/tests/test.js
+++ api/tests/test.js
@@ -1,8 +1,50 @@
-var assert = require("assert");
-describe("Array", function() {
-  describe("#indexOf()", function() {
-    it("should return -1 when the value is not present", function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+const app = require("../index");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+describe("Ravintola", function() {
+  describe("#haeRavintolat()", function() {
+    it("Palauttaa status 200 ja vähintään yhden rivin vastauksia", done => {
+      chai
+        .request(app)
+        .get("/api/ravintolat")
+        .then(res => {
+          expect(res).to.have.status(200);
+          expect(res.body).length.greaterThan(0);
+          done();
+        });
+    });
+  });
+});
+
+describe("Ruokalistat", function() {
+  describe("#haeListat()", function() {
+    it("Palauttaa status 200 ja vähintään yhden rivin vastauksia", done => {
+      chai
+        .request(app)
+        .get("/api/listat")
+        .then(res => {
+          expect(res).to.have.status(200);
+          expect(res.body).length.greaterThan(0);
+          done();
+        });
+    });
+    it("Testaa ulostulevat kentät", done => {
+      chai
+        .request(app)
+        .get("/api/listat")
+        .then(res => {
+          expect(res).to.have.status(200);
+          expect(res.body[0]).to.have.property("apiid");
+          expect(res.body[0]).to.have.property("paiva");
+          expect(res.body[0]).to.have.property("nimi");
+          expect(res.body[0]).to.have.property("rivi");
+          expect(res.body[0]).to.have.property("teksti");
+          done();
+        });
     });
   });
 });
