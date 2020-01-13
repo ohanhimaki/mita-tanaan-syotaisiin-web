48ca562cc4e028fb11c0fa7bcd7909a860efb07d
Olli Hanhimäki <ohanhimaki@gmail.com>
2020-01-13 21:11:32 +0200

    Lisättiin kenttien muuttaminen readonlyksi ja urli prefixin lisäys tassa.fi listoille

--- src/app/admin/add-restaurant/add-restaurant.component.ts
+++ src/app/admin/add-restaurant/add-restaurant.component.ts
@@ -5,17 +5,32 @@ import { AdminService } from '../admin.service';
 @Component({
   selector: 'app-add-restaurant',
   templateUrl: './add-restaurant.component.html',
-  styleUrls: ['./add-restaurant.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./add-restaurant.component.scss']
 })
 export class AddRestaurantComponent implements OnInit {
   @Output() refreshRestaurants: EventEmitter<any> = new EventEmitter();
   restaurant = new Restaurant();
+
   apikey = '';
+  apiRestaurantUrlPrefix = 'https://tassa.fi/kohde/';
   constructor(private _api: AdminService) { }
 
   ngOnInit() {
   }
+  changeLink(event?) {
+    console.log(event);
+
+    if (this.restaurant.tassalista) {
+
+      this.restaurant.linkki = this.apiRestaurantUrlPrefix + this.restaurant.apiid;
+    } else {
+      console.log(this.restaurant.tassalista);
+
+      this.restaurant.apiid = 0;
+      this.restaurant.linkki = '';
+    }
+  }
+
   addRestaurant() {
     this.restaurant.tassalista = this.restaurant.tassalista ? 1 : 0;
     const response = this._api.addRestaurant(this.restaurant, this.apikey);
@@ -25,6 +40,7 @@ export class AddRestaurantComponent implements OnInit {
     });
     response.then(() => {
       alert('Ravintola lisättiin');
+      this.restaurant = new Restaurant();
       this.refreshRestaurants.emit(null);
     });
 
