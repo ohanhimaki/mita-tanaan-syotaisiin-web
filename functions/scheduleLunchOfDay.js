8ff37f5b7a5a483e12db5b79472fe779f7b7c1e8
Olli Hanhimäki <39125910+ohanhimaki@users.noreply.github.com>
2022-11-26 21:18:36 +0200

    Ajat UTC, eli 2h taaksepäin

--- functions/scheduleLunchOfDay.js
+++ functions/scheduleLunchOfDay.js
@@ -3,7 +3,7 @@ const generateLunchOfDay = require('./GenerateLunchOfDay')
 
 // To learn about scheduled functions and supported cron extensions,
 // see: https://ntl.fyi/sched-func
-module.exports.handler = schedule("45 10 * * *", async (event) => {
+module.exports.handler = schedule("45 8 * * *", async (event) => {
   const eventBody = JSON.parse(event.body)
   console.log(`Next function run at ${eventBody.next_run}.`)
   return await generateLunchOfDay.handler(event);
