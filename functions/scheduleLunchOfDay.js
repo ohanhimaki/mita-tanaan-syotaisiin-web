64e8cb9e348286188e637ac2262ad0d823163f85
Olli Hanhimäki <39125910+ohanhimaki@users.noreply.github.com>
2023-03-30 12:50:55 +0300

    Update scheduleLunchOfDay.js
    Kesäaika

--- functions/scheduleLunchOfDay.js
+++ functions/scheduleLunchOfDay.js
@@ -3,7 +3,7 @@ const generateLunchOfDay = require('./GenerateLunchOfDay')
 
 // To learn about scheduled functions and supported cron extensions,
 // see: https://ntl.fyi/sched-func
-module.exports.handler = schedule("58 8 * * *", async (event) => {
+module.exports.handler = schedule("58 7 * * *", async (event) => {
   const eventBody = JSON.parse(event.body)
 
   console.log("Calling generateLunchOfDay ");
